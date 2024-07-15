45af21c95d741b0285bf05329c1eb33f3f60b904
Hashit <raizadaharshit2004@gmail.com>
2024-07-15 21:57:23 +0530

    ok

--- backend/dist/utils/validator.js
+++ backend/dist/utils/validator.js
@@ -1,6 +1,22 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_validator_1 = require("express-validator");
+const validate = (validations) => {
+    return (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+        for (let validation of validations) {
+            const result = yield validation.run(req);
+        }
+    });
+};
 const signupValidator = [
     (0, express_validator_1.body)("name").notEmpty().withMessage("Name is required"),
     (0, express_validator_1.body)("email").notEmpty().trim().isEmail().withMessage("Email is required"),
--- backend/src/utils/validator.ts
+++ backend/src/utils/validator.ts
@@ -1,5 +1,13 @@
-import { body } from "express-validator";
+import { NextFunction, Request, Response } from "express";
+import { body, ValidationChain } from "express-validator";
 
+const validate = (validations: ValidationChain[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    for (let validation of validations) {
+      const result = await validation.run(req);
+    }
+  };
+};
 const signupValidator = [
   body("name").notEmpty().withMessage("Name is required"),
   body("email").notEmpty().trim().isEmail().withMessage("Email is required"),
