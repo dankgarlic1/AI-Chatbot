4de1305b13426e20f3a8d58f0bab68c9783ed27c
Hashit <raizadaharshit2004@gmail.com>
2024-07-19 15:36:44 +0530

    auth context done

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Toolbar from "@mui/material/Toolbar";
 import AppBar from "@mui/material/AppBar";
+import Logo from "./shared/Logo";
 
 const Header = () => {
   return (
     <AppBar
       sx={{ bgcolor: "transparent", boxShadow: "none", position: "static" }}
     >
-      <Toolbar sx={{ display: "flex" }} />
+      <Toolbar sx={{ display: "flex" }}>
+        <Logo />
+      </Toolbar>
     </AppBar>
   );
 };
