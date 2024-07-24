e4b5aec01b1a9e672be382b3785c4ac6f9e8b828
Hashit <raizadaharshit2004@gmail.com>
2024-07-24 08:31:55 +0530

    chat api done

--- backend/src/config/openai-config.ts
+++ backend/src/config/openai-config.ts
@@ -2,14 +2,8 @@
 import { OpenAI } from "openai";
 import dotenv from "dotenv";
 dotenv.config();
-export const configureOpenAI = () => {
-  //   const config = new opneai({
-  //     apiKey: process.env.OPENAI_SECRET_KEY,
-  //     organization: process.env.OPENAI_ORGANIZATION_KEY,
-  //   });
-  //   return config;
-  return new OpenAI({
-    apiKey: process.env.OPENAI_SECRET_KEY,
-    organization: process.env.OPENAI_ORGANIZATION_KEY,
-  });
-};
+
+export const Config = new OpenAI({
+  apiKey: process.env.OPENAI_SECRET_KEY,
+  organization: process.env.OPENAI_ORGANIZATION_KEY,
+});
--- backend/src/controllers/chat-controllers.ts
+++ backend/src/controllers/chat-controllers.ts
@@ -1,12 +1,12 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/User";
-import { configureOpenAI } from "../config/openai-config";
+
 import { OpenAI } from "openai";
-import {
-  ChatCompletionMessage,
-  ChatCompletionMessageParam,
-  ChatCompletionUserMessageParam,
-} from "openai/resources";
+import { ChatCompletionMessageParam } from "openai/resources";
+import dotenv from "dotenv";
+import { Config } from "../config/openai-config";
+import { error, log } from "console";
+dotenv.config();
 
 export const generateChatCompletion = async (
   req: Request,
@@ -14,37 +14,52 @@ export const generateChatCompletion = async (
   next: NextFunction
 ) => {
   const { message } = req.body;
-  const user = await User.findById(res.locals.jwtData.id);
-  if (!user) {
-    return res
-      .status(401)
-      .json({ message: "User not registered or Token malfunctionded" });
+  try {
+    const user = await User.findById(res.locals.jwtData.id);
+    if (!user) {
+      return res
+        .status(401)
+        .json({ message: "User not registered or Token malfunctionded" });
+    }
+    //grab the chats of user
+    type UserChat = {
+      role: "user" | "assistant" | "system";
+      content: string;
+    };
+
+    // Convert the user's chats to the correct format
+    const userChat: ChatCompletionMessageParam[] = user.chats.map(
+      (chat: any) => {
+        const role: "user" | "assistant" | "system" = chat.role as
+          | "user"
+          | "assistant"
+          | "system";
+        const content: string = chat.content;
+        return { role, content };
+      }
+    );
+    userChat.push({ content: message, role: "user" });
+    user.chats.push({ content: message, role: "user" }); //pushing it in user chats also
+    //send all the chats with new one to openai api
+
+    const openai = new OpenAI({
+      apiKey: process.env.OPENAI_SECRET_KEY,
+      organization: process.env.OPENAI_ORGANIZATION_KEY,
+    });
+    //get latest response
+
+    const chatResponse = await openai.chat.completions.create({
+      model: "gpt-3.5.turbo",
+      messages: userChat,
+    });
+    const latestResponse: any = chatResponse.choices[0].message.content;
+
+    user.chats.push(latestResponse);
+    await user.save();
+    return res.status(200).json({ chats: user.chats });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({ message: "Something went wrong" });
   }
-  //grab the chats of user
-  type UserChat = {
-    role: "user" | "assistant" | "system";
-    content: string;
-  };
-
-  // Convert the user's chats to the correct format
-  const userChat: ChatCompletionMessageParam[] = user.chats.map((chat: any) => {
-    const role: "user" | "assistant" | "system" = chat.role as
-      | "user"
-      | "assistant"
-      | "system";
-    const content: string = chat.content;
-    return { role, content };
-  });
-  userChat.push({ content: message, role: "user" });
-  user.chats.push({ content: message, role: "user" }); //pushing it in user chats also
-  //send all the chats with new one to openai api
-
-  const config: OpenAI = configureOpenAI();
-  const openai = new OpenAI(config);
-  const chatResponse = await openai.chat.completions.create({
-    model: "gpt-3.5.turbo",
-    messages: userChat,
-  });
-
-  //get latest response
 };
